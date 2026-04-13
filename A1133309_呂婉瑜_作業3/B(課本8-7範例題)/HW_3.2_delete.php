8f6b64e26f56032b1b7248caae6af973f4b902d6
Wanyu <cherry94121111@gmail.com>
2026-04-13 18:23:55 +0800

    Delete A1133309_呂婉瑜_作業3/B(課本8-7範例題) directory

--- A1133309_呂婉瑜_作業3/B(課本8-7範例題)/HW_3.2_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $id = $_GET["id"];
-    if (isset($id)) {
-        setcookie("Cart[" . $id . "]", "", time() - 3600);
-    }
-    header("Refresh:0;url=HW_3.2_shoppingcart.php");
-    exit();
-?>
